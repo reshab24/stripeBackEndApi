c512a9043920da9e0d1314923a93a942b75c8425
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2021-01-21 13:06:26 +0530

    Fixed express issue

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ const app = express()
 
 
 app.use(cors())
+
+app.use('/',express.static(path.join(__dirname, 'angular')))
+
+app.use('/', (_, res)=>{
+    res.sendFile(path.join(__dirname,"angular","index.html"));
+})
 // app.use(express.json())
 
 // app.use('/users', usersRouter)
 // app.use('/auth',authRouter)
 app.use('/createPayment',paymentRouter)
+
 
 module.exports=app;
--- server.js
+++ server.js
@@ -2,10 +2,6 @@ require('dotenv').config();
 
 const app=require('./app');
 
-app.use('/app',express.static(path.join(__dirname, 'angular')))
-
-res.sendFile(path.join(__dirname,"angular","index.html"));
-
 const port =process.env.PORT || 4000;
 
 app.listen(port, () => {
