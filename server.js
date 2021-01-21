ab4feff5c5c33cf721496815f0e0897d15fb1555
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2021-01-21 12:49:40 +0530

    includedDist

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const app=require('./app');
 
+app.use('/app',express.static(path.join(__dirname, 'angular')))
+
+res.sendFile(path.join(__dirname,"angular","index.html"));
+
 const port =process.env.PORT || 4000;
 
 app.listen(port, () => {
